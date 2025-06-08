ca988177340a800aa3a680e6c5bc874cacf57658
NIKHIL MUSIC REPO <vikashkohli7690@gmail.com>
2025-06-08 15:56:18 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -34,10 +34,10 @@ async def start(event):
         bot_name = AltBot.first_name
         bot_id = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ](https://t.me/Shivam_jaiswal0011)**\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ](https://t.me/ll_YOUR_FATHER_HU_ll)**\n\n"
         TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
-        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━\n\n🥀 ᴛʜɪs ɪs sᴘᴀᴍ ʙᴏᴛ ᴅᴏɴ'ᴛ ᴛʜɪɴᴋ I ᴀᴍ ᴏᴡɴᴇʀ [ɪ ᴀᴍ Jᴜsᴛ ᴄᴏᴅᴇ ᴅᴇᴘʟᴏᴇʀ](https://t.me/Shivam_jaiswal0011) , ᴜsᴇ sᴘᴀᴍʙᴏᴛ ᴀɴᴅ ᴇɴJᴏʏ"
+        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━\n\n🥀 ᴛʜɪs ɪs sᴘᴀᴍ ʙᴏᴛ ᴅᴏɴ'ᴛ ᴛʜɪɴᴋ I ᴀᴍ ᴏᴡɴᴇʀ [ɪ ᴀᴍ Jᴜsᴛ ᴄᴏᴅᴇ ᴅᴇᴘʟᴏᴇʀ](https://t.me/ll_YOUR_FATHER_HU_ll) , ᴜsᴇ sᴘᴀᴍʙᴏᴛ ᴀɴᴅ ᴇɴJᴏʏ"
         await event.client.send_file(
                     event.chat_id,
                     "https://te.legra.ph/file/797c015dc0def4448716b.jpg",
